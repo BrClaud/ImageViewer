58627796f95946ab88f6bf7cb94adb4516e419e4
dorothyz <urazaevdanil0@gmail.com>
2025-03-10 15:49:14 +0300

    .h

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,96 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setMouseTracking(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_button_load_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(this, "Open Image", "", "Images (*.png *.jmg *.bmp" );
+    if(!path.isEmpty()){
+        loadImage(path);
+        updateDisplay();
+    }
+
+}
+
+void MainWindow::on_checkBox_stateChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_button_plus_clicked()
+{
+
+}
+
+void MainWindow::on_button_minus_clicked()
+{
+
+}
+
+void MainWindow::loadImage(const QString& path){
+    originalImage.load(path);
+    if(originalImage.isNull()){
+        qWarning()<<"Failed to load image: "<<path<<"\n";
+    }
+
+    imageOffset = QPointF(0,0);
+    imageScale = 1.0;
+    savedRects.clear();
+}
+
+// Преобразование координат виджета в координаты изображения
+QPointF MainWindow::widgetToImagePos(const QPointF& widgetPos) const{
+    return (widgetPos - imageOffset) / imageScale;
+}
+
+
+
+
+void MainWindow::paintEvent(QPaintEvent* event) {
+    QMainWindow::paintEvent(event);
+    QPainter painter(this);
+
+    if (!originalImage.isNull()) {
+        // Рисуем изображение с учетом масштаба и смещения
+        painter.save();
+        painter.translate(imageOffset);
+        painter.scale(imageScale, imageScale);
+        painter.drawPixmap(0, 0, originalImage);
+        painter.restore();
+
+        // Рисуем все сохраненные прямоугольники
+        painter.setPen(QPen(Qt::red,2));
+
+        for (const QRectF& rect : savedRects) {
+            QRectF scaledRect = rect;
+            scaledRect.moveTo(rect.topLeft() * imageScale + imageOffset);
+            scaledRect.setSize(rect.size() * imageScale);
+            painter.drawRect(scaledRect);
+        }
+
+        if(isDrawing){
+            painter.setPen(QPen(Qt::blue,2));
+            QRectF scaledRect = currentRect;
+            scaledRect.moveTo(currentRect.topLeft() * imageScale + imageOffset);
+            scaledRect.setSize(currentRect.size() * imageScale);
+            painter.drawRect(scaledRect);
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPixmap>
+#include <QPointF>
+#include <QRectF>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +20,35 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_button_load_clicked();
+    void on_checkBox_stateChanged(int arg1);
+    void on_button_plus_clicked();
+    void on_button_minus_clicked();
+
+protected:
+    // обработчики
+    void paintEvent(QPaintEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+
 private:
+    void loadImage(const QString& path);
+    QPointF widgetToImagePos(const QPointF& widgetPos) const;
+    void updateDisplay();
+    void startDrawing(const QPointF& pos);
+    void updateDrawing(const QPointF& pos);
+    void finishDrawing();
+
     Ui::MainWindow *ui;
+    QPixmap originalImage;    // Исходное изображение
+    QPointF imageOffset;      // Смещение изображения
+    qreal imageScale = 1.0;   // Текущий масштаб (1.0 = 100%)
+    bool isPanning = false;   // Флаг перемещения изображения
+    bool isDrawing = false;   // Флаг режима рисования
+    QPointF lastMousePos;     // Последняя позиция мыши
+    QRectF currentRect;       // Текущий прямоугольник
+    QList<QRectF> savedRects; // Сохраненные прямоугольники
 };
 #endif // MAINWINDOW_H
